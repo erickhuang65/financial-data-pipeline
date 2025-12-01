1e7c3721ed7b4722f73a468deef101036b9a90a3
Erick <erickchhuang@gmail.com>
2025-11-30 20:36:52 -0800

    load dataframe method

--- src/bigquery_connector.py
+++ src/bigquery_connector.py
@@ -2,6 +2,7 @@ import logging
 import os
 from google.cloud import bigquery
 from google.api_core import exceptions
+from google.api_core.exceptions import GoogleAPIError
 import pandas as pd
 
 class BigQueryConnector:
@@ -38,38 +39,32 @@ class BigQueryConnector:
                 project=project_id
             )
             self.logger.info(f"BigQuery client initialized for project: {project_id}")
-        except exceptions.GoogleCloudError as e:
+        except GoogleAPIError as e:
             self.logger.error(f"Failed to initialize BigQuery client: {str(e)}")
             raise
         
 # 1. Setup: 
 
-# create_dataset() - create dataset if it doesn't exist
-# create_table() - create tables with proper schemas
-
     def create_dataset(
             self,
-            dataset＿id: str, 
-            location: str = "US", 
+            dataset_id: str, 
+            location: str = "US",
             exists_ok: bool = True):
         """
         Creates a BigQuery dataset if the table doesn't exist.
         Args:
             dataset_id: Dataset ID (e.g., stock_market_data)
             location: Geographic location to default US
-            exists_ok: If t
+            exists_ok: 
         """
         try:
             dataset = self.client.create_dataset(dataset, exists_ok=exists_ok)
             self.logger.info(f"Dataset {dataset_id} created or already exists")
             return dataset
-        except exceptions.GoogleCloudError as e:
+        except GoogleAPIError as e:
             self.logger.error(f"Error creating dataset {dataset_id}: {e}")
             raise
         
-    def create_table(self):
-        pass
-
 # 2. Data Loading Operations
     # load pandas DataFrame to table
     def load_dataframe(self, df: pd.DataFrame, destination:str, job_config=None):
@@ -81,29 +76,29 @@ class BigQueryConnector:
             
         Returns:
             a LoadJob Object
-        """
+        """ 
         # if df is None:
         #     self.logger.error(f"DataFrame cannot be None")
         #     return False
 
         if df.empty:
             self.logger.error(f"DataFrame is empty")
-            return ValueError(f"Cannot load empty DataFrame")
+            raise ValueError(f"Cannot load empty DataFrame")
         
         if not destination:
             self.logger.error(f"Missing destination table")
-            return ValueError(f"Destination table required")
+            raise ValueError(f"Destination table required")
         
         job_config = bigquery.LoadJobConfig()
         job_config.write_disposition = 'WRITE_APPEND'
         
         try:
             job = self.client.load_table_from_dataframe(
-                dataframe = df, 
+                dataframe = df,
                 destination = destination, 
                 job_config = job_config)
             job.result()
-        except exceptions.GoogleCloudError as e:
+        except GoogleAPIError as e:
             self.logger.error(f"Error loading dataframe to BigQuery Client: {e}")
 
     # insert data in batches for efficiency
